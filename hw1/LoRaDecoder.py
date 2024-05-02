5402d01bb7c727249c8261242254fee23d2c4437
xsun2445 <xinghua@uw.edu>
2024-05-01 23:00:55 -0700

    hw1

--- hw1/LoRaDecoder.py
+++ hw1/LoRaDecoder.py
@@ -8,16 +8,28 @@ from functools import reduce
 
 
 class LoRaDecoder():
-    def __init__(self, sf, coding_para=None) -> None:
-        self.coding_para = coding_para
+    def __init__(self, sf, plen=16, cp=6) -> None:
 
         self.fs = 250*1e3
+        self.fs = self.fs/2
         self.bw = 125*1e3
 
         self.sf = sf
         self.cfo = 0
-        self.cp = 6
+        self.cp = cp
         self.ldr = 0
+        self.preamble_len = 8
+        self.upsample_enable = False
+        self.crc_enable = 1
+        self.header_enable = 0
+
+        self.samp_per_symbol = int(self.fs*2**self.sf//self.bw)
+        self.num_bins = 2**self.sf
+        # payload length is always 16 bytes
+        self.payload_len = plen
+        self.symbol_len = self.calc_symbol_num()
+        # print(self.symbol_len)
+
 
 
         # self.whitening_seq = np.array([0xff, 0xfe, 0xfc, 0xf8, 0xf0, 0xe1, 0xc2, 0x85, 0xb, 0x17, 0x2f, 0x5e, 0xbc, 0x78, 0xf1, 0xe3, 0xc6, 0x8d, 0x1a, 0x34, 0x68, 0xd0, 0xa0, 0x40, 0x80, 0x1, 0x2, 0x4, 0x8, 0x11, 0x23, 0x47, 0x8e, 0x1c, 0x38, 0x71, 0xe2, 0xc4, 0x89, 0x12, 0x25, 0x4b, 0x97, 0x2e, 0x5c, 0xb8, 0x70, 0xe0, 0xc0, 0x81, 0x3, 0x6, 0xc, 0x19, 0x32, 0x64, 0xc9, 0x92, 0x24, 0x49, 0x93, 0x26, 0x4d, 0x9b, 0x37, 0x6e, 0xdc, 0xb9, 0x72, 0xe4, 0xc8, 0x90, 0x20, 0x41, 0x82, 0x5, 0xa, 0x15, 0x2b, 0x56, 0xad, 0x5b, 0xb6, 0x6d, 0xda, 0xb5, 0x6b, 0xd6, 0xac, 0x59, 0xb2, 0x65, 0xcb, 0x96, 0x2c, 0x58, 0xb0, 0x61, 0xc3, 0x87, 0xf, 0x1f, 0x3e, 0x7d, 0xfb, 0xf6, 0xed, 0xdb, 0xb7, 0x6f, 0xde, 0xbd, 0x7a, 0xf5, 0xeb, 0xd7, 0xae, 0x5d, 0xba, 0x74, 0xe8, 0xd1, 0xa2, 0x44, 0x88, 0x10, 0x21, 0x43, 0x86, 0xd, 0x1b, 0x36, 0x6c, 0xd8, 0xb1, 0x63, 0xc7, 0x8f, 0x1e, 0x3c, 0x79, 0xf3, 0xe7, 0xce, 0x9c, 0x39, 0x73, 0xe6, 0xcc, 0x98, 0x31, 0x62, 0xc5, 0x8b, 0x16, 0x2d, 0x5a, 0xb4, 0x69, 0xd2, 0xa4, 0x48, 0x91, 0x22, 0x45, 0x8a, 0x14, 0x29, 0x52, 0xa5, 0x4a, 0x95, 0x2a, 0x54, 0xa9, 0x53, 0xa7, 0x4e, 0x9d, 0x3b, 0x77, 0xee, 0xdd, 0xbb, 0x76, 0xec, 0xd9, 0xb3, 0x67, 0xcf, 0x9e, 0x3d, 0x7b, 0xf7, 0xef, 0xdf, 0xbf, 0x7e, 0xfd, 0xfa, 0xf4, 0xe9, 0xd3, 0xa6, 0x4c, 0x99, 0x33, 0x66, 0xcd, 0x9a, 0x35, 0x6a, 0xd4, 0xa8, 0x51, 0xa3, 0x46, 0x8c, 0x18, 0x30, 0x60, 0xc1, 0x83, 0x7, 0xe, 0x1d, 0x3a, 0x75, 0xea, 0xd5, 0xaa, 0x55, 0xab, 0x57, 0xaf, 0x5f, 0xbe, 0x7c, 0xf9, 0xf2, 0xe5, 0xca, 0x94, 0x28, 0x50, 0xa1, 0x42, 0x84, 0x9, 0x13, 0x27, 0x4f, 0x9f, 0x3f, 0x7f]).astype(np.uint8)
@@ -25,8 +37,56 @@ class LoRaDecoder():
         self.upchirp = self.chirp(True, sf, self.bw, self.fs, 0, 0, 0)
         self.downchirp = self.chirp(False, sf, self.bw, self.fs, 0, 0, 0)
 
+
+
+    def demodulate(self, raw):
+        # the input raw data should has fs 125e3
+        loc_start, self.cfo = self.start_detection(raw)
+        # always 16 bytes payload, then always self.symbol_len symbols
+        chirp_list = raw[loc_start:loc_start+self.symbol_len*self.samp_per_symbol] 
+        # correct the data with cfo
+        chirp_list *= np.exp(-1j*2*np.pi*self.cfo*(1/self.fs)*np.arange(len(chirp_list)))
+
+        # reshape s.t. each row is a chirp
+        chirp_list = chirp_list.reshape(-1, self.samp_per_symbol)
+        fftData = np.fft.fft(chirp_list*self.downchirp, axis=1)
+        # print(fftData.shape)
+        symbols = np.argmax(fftData, axis=1)
+        # print(symbols)
+        # print(symbols.shape)
+        return symbols
+
+
+    def start_detection(self, raw):
+        preamble = np.matlib.repmat(self.downchirp, 1, self.preamble_len).flatten()
+        # since the netid is unknown, so just avoid multiply it
+        netid = np.zeros(2*self.samp_per_symbol)
+        sync = np.concatenate((self.upchirp,self.upchirp,self.upchirp[:len(self.upchirp)//4]))
+        preamble = np.concatenate((preamble, netid, sync))
+        max_val = 0
+        loc_time = 0
+        loc_freq = 0
+        for i in range(len(raw)-len(preamble)):
+            temp_fft = abs(np.fft.fft(raw[i:i+len(preamble)]*preamble))
+            # if self.enable_upsample:
+                # temp_fft = temp_fft[:len(temp_fft)//2] + temp_fft[len(temp_fft)//2:]
+            temp_freq = np.argmax(temp_fft)
+            temp_max = temp_fft[temp_freq]
+            if temp_max > max_val:
+                max_val = temp_max
+                loc_time = i
+                loc_freq = temp_freq
+        
+        loc_freq %= self.num_bins
+        loc_freq = loc_freq/len(preamble)*self.samp_per_symbol *self.fs/self.num_bins
+        loc_time += len(preamble)
+
+        return loc_time, loc_freq
+            
+
+
     
-    def decode(self, data_demod, fileName=None, len_payload=None):
+    def decode(self, data_demod):
         # grey decoding
         data_interleaved = self.gray_coding(data_demod)
 
@@ -209,8 +269,15 @@ class LoRaDecoder():
             reg = (reg<<np.uint8(1)) ^ (LoRaDecoder.getbit(reg,8) ^ (LoRaDecoder.getbit(reg,6) ^ (LoRaDecoder.getbit(reg,5) ^ LoRaDecoder.getbit(reg,4))))
         return np.array(seq).astype(np.uint8)
     
+
     def whiten(self, data_bytes):
         return np.array([x^y for x,y in zip(data_bytes, self.whitening_seq)]).astype(np.uint8)
+    
+
+    def calc_symbol_num(self):
+        plen = self.payload_len
+        res = 8 + max(self.cp*np.ceil((2*plen-self.sf+7+4*self.crc_enable-5*(1-self.header_enable))/(self.sf-2*self.ldr)), 0)
+        return int(res)
 
     
     @staticmethod
@@ -288,10 +355,10 @@ if __name__ == '__main__':
     
     fileName = sys.argv[1]
     plen = int(sys.argv[2])
-    coding_para = sys.argv[3]
+    cp = sys.argv[3]
 
     fileName = './EE595 Project Data/dataSF8CR8packet1.bin'
-    decoder = LoRaDecoder(coding_para)
+    decoder = LoRaDecoder(sf, plen, cp)
     data = decoder.read_complex(fileName)
     print(type(data))
     print(data.dtype)
@@ -306,27 +373,7 @@ if __name__ == '__main__':
 
 
 
-    # plt.figure()
-    # plt.plot(np.abs(data))
-    # plt.show()
 
-    # plt.figure()
-    # plt.plot(data.real)
-    # plt.show()
-
-    # plt.figure()
-    # plt.plot(np.fft.fft(data))
-    # plt.title('fft')
-    # plt.show()
-
-    # import matplotlib.pyplot as plt
-    # t = np.arange(256)
-    # sp = np.fft.fft(np.sin(t))
-    # freq = np.fft.fftfreq(t.shape[-1])
-    # plt.plot(freq, sp.real, freq, sp.imag)
-    # plt.show()
-
-    # print(np.fft.fft(np.exp(2j * np.pi * np.arange(8) / 8)))
 
 
     
